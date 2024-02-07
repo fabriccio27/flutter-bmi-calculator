cc94efad9d4fed6c6cc9ffe1629ce018e7ed517d
fabriccio27 <fabricio_inc27@hotmail.com>
2024-02-07 16:02:46 -0300

    Add cardChild prop to ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,6 +5,10 @@ class InputPage extends StatefulWidget {
   _InputPageState createState() => _InputPageState();
 }
 
+const reusableCardColor = Color(0xFF1D1E33);
+const bottonContainerColor = Color(0xFFEB1555);
+const bottonContainerHeight = 80.0;
+
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
@@ -19,60 +23,54 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(colour: reusableCardColor),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(colour: reusableCardColor),
                 )
               ],
             )
           ),
           Expanded(
-            child: Container(
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10)
-              ),
-            )
+            child: ReusableCard(colour: reusableCardColor)
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(colour: reusableCardColor),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
+                  child: ReusableCard(colour: reusableCardColor),
                 )
               ],
             )
           ),
+          Container(
+              color: bottonContainerColor,
+              margin: const EdgeInsetsDirectional.only(top: 10.0),
+              width: double.infinity,
+              height: bottonContainerHeight,
+          ),
         ]
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required Color this.colour, required Widget this.cardChild });
+  final Color colour;
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10)
       ),
     );
   }
